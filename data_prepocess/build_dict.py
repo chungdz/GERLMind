10949f062e7221a2c2eb91d3f46297de39c7f388
Yunfan Hu <1033718596@qq.com>
2021-01-13 17:14:11 +0800

    first version build dict

---
+++ data_prepocess/build_dict.py
@@ -0,0 +1,118 @@
+import os
+import json
+import pickle
+import argparse
+import re
+import pandas as pd
+import numpy as np
+
+def parse_ent_list(x):
+    if x.strip() == "":
+        return ''
+
+    return ' '.join([k["WikidataId"] for k in json.loads(x)])
+
+punctuation = '!,;:?"\''
+def removePunctuation(text):
+    text = re.sub(r'[{}]+'.format(punctuation),'',text)
+    return text.strip().lower()
+
+data_path = '../data'
+
+print("Loading news info")
+f_train_news = os.path.join(data_path, "train/news.tsv")
+f_dev_news = os.path.join(data_path, "valid/news.tsv")
+f_test_news = os.path.join(data_path, "test/news.tsv")
+
+print("Loading training news")
+all_news = pd.read_csv(f_train_news, sep="\t", encoding="utf-8",
+                        names=["newsid", "cate", "subcate", "title", "abs", "url", "title_ents", "abs_ents"],
+                        quoting=3)
+
+print("Loading dev news")
+dev_news = pd.read_csv(f_dev_news, sep="\t", encoding="utf-8",
+                        names=["newsid", "cate", "subcate", "title", "abs", "url", "title_ents", "abs_ents"],
+                        quoting=3)
+all_news = pd.concat([all_news, dev_news], ignore_index=True)
+print("Loading testing news")
+test_news = pd.read_csv(f_test_news, sep="\t", encoding="utf-8",
+                        names=["newsid", "cate", "subcate", "title", "abs", "url", "title_ents", "abs_ents"],
+                        quoting=3)
+all_news = pd.concat([all_news, test_news], ignore_index=True)
+all_news = all_news.drop_duplicates("newsid")
+
+news_dict = {}
+word_dict = {'<pad>': 0}
+word_idx = 1
+news_idx = 2
+for n, title in all_news[['newsid', "title"]].values:
+    news_dict[n] = {}
+    news_dict[n]['idx'] = news_idx
+    news_idx += 1
+
+    tarr = removePunctuation(title).split()
+    wid_arr = []
+    for t in tarr:
+        if t not in word_dict:
+            word_dict[t] = word_idx
+            word_idx += 1
+        wid_arr.append(word_dict[t])
+    cur_len = len(wid_arr)
+    if cur_len < 10:
+        for l in range(10 - cur_len):
+            wid_arr.append(0)
+    news_dict[n]['title'] = wid_arr[:10]
+
+## paddning news for impression
+news_dict['<pad>']= {}
+news_dict['<pad>']['idx'] = 0
+tarr = removePunctuation("This is the title of the padding news").split()
+wid_arr = []
+for t in tarr:
+    if t not in word_dict:
+        word_dict[t] = word_idx
+        word_idx += 1
+    wid_arr.append(word_dict[t])
+cur_len = len(wid_arr)
+if cur_len < 10:
+    for l in range(10 - cur_len):
+        wid_arr.append(0)
+news_dict['<pad>']['title'] = wid_arr[:10]
+## paddning news for history
+news_dict['<his>']= {}
+news_dict['<his>']['idx'] = 1
+news_dict['<his>']['title'] = list(np.zeros(10))
+
+print('all word', len(word_dict))
+print('all news', len(news_dict))
+json.dump(news_dict, open('data/news.json', 'w', encoding='utf-8'))
+json.dump(word_dict, open('data/word.json', 'w', encoding='utf-8'))
+
+print("Loading behaviors info")
+f_train_beh = os.path.join(data_path, "train/behaviors.tsv")
+f_dev_beh = os.path.join(data_path, "valid/behaviors.tsv")
+f_test_beh = os.path.join(data_path, "test/behaviors.tsv")
+
+print("Loading training beh")
+all_beh = pd.read_csv(f_train_beh, sep="\t", encoding="utf-8", names=["id", "uid", "time", "hist", "imp"])
+print("Loading dev beh")
+dev_beh = pd.read_csv(f_dev_beh, sep="\t", encoding="utf-8", names=["id", "uid", "time", "hist", "imp"])
+all_beh = pd.concat([all_beh, dev_beh], ignore_index=True)
+print("Loading testing beh")
+test_beh = pd.read_csv(f_test_beh, sep="\t", encoding="utf-8", names=["id", "uid", "time", "hist", "imp"])
+all_beh = pd.concat([all_beh, test_beh], ignore_index=True)
+all_beh = all_beh.drop_duplicates("id")
+print("All beh: {}".format(len(all_beh)))
+
+user_ids = pd.unique(all_beh['uid'])
+user_dict = {}
+user_idx = 1
+for u in user_ids:
+    user_dict[u] = {}
+    user_dict[u]['idx'] = user_idx
+    user_idx += 1
+
+user_dict['<pad>'] = {}
+user_dict['<pad>']['idx'] = 0
+
+json.dump(user_dict, open('data/user.json', 'w', encoding='utf-8'))
---
+++ utils/log_util.py
@@ -0,0 +1,64 @@
+# -*- encoding:utf-8 -*-
+"""
+fork from https://github.com/cs230-stanford/cs230-code-examples
+"""
+import logging
+
+NEPTUNE_API_TOKEN = "eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vdWkubmVwdHVuZS5haSIsImFwaV91cmwiOiJodHRwczovL3VpLm5lcHR1bmUuYWkiLCJhcGlfa2V5IjoiZGQyMjU4YjYtZTQyZC00OWYyLWFmOTYtZWY1ZGY3YzI5MTM3In0="
+
+
+def set_logger(log_path):
+    """Set the logger to log info in terminal and file `log_path`.
+
+    In general, it is useful to have a logger so that every output to the terminal is saved
+    in a permanent file. Here we save it to `model_dir/train.log`.
+
+    Example:
+    ```
+    logging.info("Starting training...")
+    ```
+
+    Args:
+        log_path: (string) where to log
+    """
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    if not logger.handlers:
+        # Logging to a file
+        file_handler = logging.FileHandler(log_path)
+        file_handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s: %(message)s'))
+        logger.addHandler(file_handler)
+
+        # Logging to console
+        stream_handler = logging.StreamHandler()
+        stream_handler.setFormatter(logging.Formatter('%(asctime)s: %(message)s'))
+        logger.addHandler(stream_handler)
+
+
+def convert_dataset_config(cfg, d):
+    d['dataset.name'] = cfg.dataset.name
+
+
+def convert_model_config(cfg, d):
+    d['model.name'] = cfg.model.name
+    d['model.dropout'] = cfg.model.dropout
+    d['model.neg_count'] = cfg.model.neg_count
+
+
+def convert_training_config(cfg, d):
+    d['train.name'] = cfg.training.name
+    d['train.batch_size'] = cfg.training.batch_size
+    d['train.accumulate'] = cfg.training.accumulate
+    d['train.learning_rate'] = cfg.training.learning_rate
+    d['train.epochs'] = cfg.training.epochs
+    d['train.seed'] = cfg.training.seed
+
+
+def convert_omegaconf_to_dict(cfg):
+    d = dict()
+    convert_dataset_config(cfg, d)
+    convert_model_config(cfg, d)
+    convert_training_config(cfg, d)
+
+    return d
