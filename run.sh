02e9da14f010919637ea267f69c494e470f6d56f
Yunfan Hu <1033718596@qq.com>
2021-01-14 17:19:53 +0800

    train

--- run.sh
+++ run.sh
@@ -1,9 +1,14 @@
 mkdir data checkpoint result
 cd data 
 mkdir train valid test raw
+cd ../
 python -m data_prepocess.build_dict
 python -m data_prepocess.find_neighbors
 python -m data_prepocess.build_train --processes=10
-python build_valid.py --processes=10 --max_hist_length 30 --fsamples=valid/behaviors.small.tsv
-python build_test.py --processes=40 --max_hist_length 30
-python resplit.py --filenum 10
+python -m data_prepocess.build_valid --processes=10 --fsamples=valid/behaviors.small.tsv
+python -m data_prepocess.build_test --processes=40
+python -m data_prepocess.resplit --filenum 10
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 python training.py --gpus=4 --epoch=4
+CUDA_VISIBLE_DEVICES=0,1,2,3 python validate.py --gpus=4 --epoch=0 --filenum=40
+
