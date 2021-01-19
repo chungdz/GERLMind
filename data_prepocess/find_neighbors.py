3144a2242b76a230dcb46e9b7057cc83ed66e89a
Yunfan Hu <1033718596@qq.com>
2021-01-19 14:50:35 +0800

    judge

--- data_prepocess/find_neighbors.py
+++ data_prepocess/find_neighbors.py
@@ -15,64 +15,76 @@ with open('data/user.pkl', 'rb') as f:
 with open('data/news.pkl', 'rb') as f2:
     news_dict = pickle.load(f2)
 
-total_neighbor_user = []
-for u, info in tqdm(user_dict.items(), total=len(user_dict), desc='user neighbor'):
-    if len(info['clicked']) < 1:
-        neighbor_user_list = []
-        for t in range(D):
-            neighbor_user_list.append(user_dict['<pad>']['idx'])
-        info['neighbor'] = neighbor_user_list
-        continue
-    
-    neighbor_user_dict = {}
-    for n in info['clicked'][-5:]:
-        if len(news_dict[n]['clicked']) > 5:
-            cur_ulist = random.sample(news_dict[n]['clicked'], 5)
-        else:
-            cur_ulist = news_dict[n]['clicked']
-        for neighbor_u in cur_ulist:
-            cur_idx = user_dict[neighbor_u]['idx']
-            if cur_idx not in neighbor_user_dict:
-                neighbor_user_dict[cur_idx] = 1
+parser.add_argument("--user", default=True, type=bool,
+                    help=" find neighbor user?")
+arser.add_argument("--news", default=True, type=bool,
+                    help="find neighbor news?")
+
+args = parser.parse_args()
+
+if args.user:
+
+    total_neighbor_user = []
+    for u, info in tqdm(user_dict.items(), total=len(user_dict), desc='user neighbor'):
+        if len(info['clicked']) < 1:
+            neighbor_user_list = []
+            for t in range(D):
+                neighbor_user_list.append(user_dict['<pad>']['idx'])
+            info['neighbor'] = neighbor_user_list
+            continue
+        
+        neighbor_user_dict = {}
+        for n in info['clicked'][-5:]:
+            if len(news_dict[n]['clicked']) > 5:
+                cur_ulist = random.sample(news_dict[n]['clicked'], 5)
             else:
-                neighbor_user_dict[cur_idx] += 1
-    
-    cur_len = len(neighbor_user_dict)
-    total_neighbor_user.append(cur_len)
-    if cur_len >= D:
-        neighbor_user_list = sorted(neighbor_user_dict, key=lambda x: -neighbor_user_dict[x])[:D]
-    else:
-        neighbor_user_list = list(neighbor_user_dict)
-        for t in range(D - cur_len):
-            neighbor_user_list.append(user_dict['<pad>']['idx'])
-    info['neighbor'] = neighbor_user_list
+                cur_ulist = news_dict[n]['clicked']
+            for neighbor_u in cur_ulist:
+                cur_idx = user_dict[neighbor_u]['idx']
+                if cur_idx not in neighbor_user_dict:
+                    neighbor_user_dict[cur_idx] = 1
+                else:
+                    neighbor_user_dict[cur_idx] += 1
+        
+        cur_len = len(neighbor_user_dict)
+        total_neighbor_user.append(cur_len)
+        if cur_len >= D:
+            neighbor_user_list = sorted(neighbor_user_dict, key=lambda x: -neighbor_user_dict[x])[:D]
+        else:
+            neighbor_user_list = list(neighbor_user_dict)
+            for t in range(D - cur_len):
+                neighbor_user_list.append(user_dict['<pad>']['idx'])
+        info['neighbor'] = neighbor_user_list
 
-print('average neighbor user', np.mean(total_neighbor_user))
+    print('average neighbor user', np.mean(total_neighbor_user))
+    with open('data/user_n.pkl', 'wb') as f3:
+        user_dict = pickle.dump(user_dict, f3)
 
-for n, info in tqdm(news_dict.items(), total=len(news_dict), desc='news neighbor'):
-    if len(info['clicked']) < 1:
-        neighbor_news_list = []
-        for t in range(D):
-            neighbor_news_list.append(news_dict['<neighbor>']['idx'])
-        info['neighbor'] = neighbor_news_list
-        continue
+if args.news:
 
-    neighbor_news_set = set()
-    for u in info['clicked']:
-        cur_nlist = user_dict[u]['clicked']
-        for neighbor_n in cur_nlist:
-            neighbor_news_set.add(news_dict[neighbor_n]['idx'])
-    
-    cur_len = len(neighbor_news_set)
-    neighbor_news_list = list(neighbor_news_set)
-    if cur_len >= D:
-        info['neighbor'] = random.sample(neighbor_news_list, D)
-    else:
-        info['neighbor'] = neighbor_news_list
-        for t in range(D - cur_len):
-            info['neighbor'].append(news_dict['<neighbor>']['idx'])
+    for n, info in tqdm(news_dict.items(), total=len(news_dict), desc='news neighbor'):
+        if len(info['clicked']) < 1:
+            neighbor_news_list = []
+            for t in range(D):
+                neighbor_news_list.append(news_dict['<neighbor>']['idx'])
+            info['neighbor'] = neighbor_news_list
+            continue
 
-with open('data/user_n.pkl', 'wb') as f3:
-    user_dict = pickle.dump(user_dict, f3)
-with open('data/news_n.pkl', 'wb') as f4:
-    news_dict = pickle.dump(news_dict, f4)
+        neighbor_news_set = set()
+        for u in info['clicked']:
+            cur_nlist = user_dict[u]['clicked']
+            for neighbor_n in cur_nlist:
+                neighbor_news_set.add(news_dict[neighbor_n]['idx'])
+        
+        cur_len = len(neighbor_news_set)
+        neighbor_news_list = list(neighbor_news_set)
+        if cur_len >= D:
+            info['neighbor'] = random.sample(neighbor_news_list, D)
+        else:
+            info['neighbor'] = neighbor_news_list
+            for t in range(D - cur_len):
+                info['neighbor'].append(news_dict['<neighbor>']['idx'])
+
+
+    with open('data/news_n.pkl', 'wb') as f4:
+        news_dict = pickle.dump(news_dict, f4)
--- run.sh
+++ run.sh
@@ -16,9 +16,9 @@ python -m data_prepocess.find_neighbors
 CUDA_VISIBLE_DEVICES=1,2,3,7 python training.py --gpus=4 --epoch=4
 
 python -m data_prepocess.build_dict --title_len=30
-python -m data_prepocess.find_neighbors
+python -m data_prepocess.find_neighbors --user=False --news=True
 python -m data_prepocess.build_train --processes=10 --max_hist_length=50
+python -m data_prepocess.resplit --filenum 10 --processes 4
 python -m data_prepocess.build_valid --processes=10 --fsamples=valid/behaviors.small.tsv --max_hist_length=50
 python -m data_prepocess.build_test --processes=40 --max_hist_length=50
-python -m data_prepocess.resplit --filenum 1 --processes 2
-CUDA_VISIBLE_DEVICES=0,1 python training.py --gpus=2 --epoch=10
+CUDA_VISIBLE_DEVICES=0,1,2,3 python training.py --gpus=4 --epoch=10 --lr=0.0005 --batch_size=256
