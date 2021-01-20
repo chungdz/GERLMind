9cc001d135d61e9bddae5b36901608b47c1f20a8
Yunfan Hu <1033718596@qq.com>
2021-01-20 12:50:35 +0800

    third version

--- models/gerl.py
+++ models/gerl.py
@@ -10,11 +10,13 @@ class GERL(nn.Module):
         self.news_transformer = Transformer(cfg)
         self.neighbor_transformer = Transformer(cfg)
         self.user_emb = nn.Embedding(cfg.user_num, cfg.ID_dim)
+        self.news_neighbor_emb = nn.Embedding(cfg.user_num, cfg.ID_dim)
         self.news_emb = nn.Embedding(cfg.news_num, cfg.ID_dim)
         self.his_news_att = AttentionLayer(cfg.hidden_size * 2)
         self.neighbor_user_att = AttentionLayer(cfg.ID_dim)
         self.neighbor_news_ID_att = AttentionLayer(cfg.ID_dim)
         self.neighbor_news_info_att = AttentionLayer(cfg.hidden_size * 2)
+        self.news_ID_att = AttentionLayer(cfg.ID_dim)
 
     def forward(self, data, test_mode=False):
         neg_num = self.cfg.neg_count
@@ -32,10 +34,14 @@ class GERL(nn.Module):
         target_user = all_user[:, 0, :].squeeze(1)
         neighbor_user = all_user[:, 1:, :]
         
-        all_news_ID = self.news_emb(all_news_ID)
-        candidates_news_ID = all_news_ID[:, :neg_num + 1, :]
-        his_news_ID = all_news_ID[:, neg_num + 1: neg_num + 1 + self.cfg.max_hist_length, :]
-        neighbor_news_ID = all_news_ID[:, neg_num + 1 + self.cfg.max_hist_length:, :].reshape(-1, self.cfg.D, self.cfg.ID_dim)
+        # all_news_ID = self.news_emb(all_news_ID)
+        candidates_news_ID = all_news_ID[:, :neg_num + 1]
+        his_news_ID = all_news_ID[:, neg_num + 1: neg_num + 1 + self.cfg.max_hist_length]
+        neighbor_news_ID = all_news_ID[:, neg_num + 1 + self.cfg.max_hist_length:]
+        
+        candidates_news_ID = self.news_emb(candidates_news_ID).reshape(-1, neg_num + 1, self.cfg.ID_dim)
+        his_news_ID = self.news_emb(his_news_ID).reshape(-1, self.cfg.max_hist_length, self.cfg.ID_dim)
+        neighbor_news_ID = self.news_neighbor_emb(neighbor_news_ID).reshape(-1, self.cfg.D, self.cfg.ID_dim)
         
         # all_news_info = self.transformer(all_news_info)
         candidcates_news_info = all_news_info[:, :neg_num + 1, :]
@@ -50,18 +56,19 @@ class GERL(nn.Module):
         nt_one = candidcates_news_info
         ne_two = self.neighbor_news_ID_att(neighbor_news_ID).reshape(-1, neg_num + 1, self.cfg.ID_dim)
         nt_two = self.neighbor_news_info_att(neighbor_news_info).reshape(-1, neg_num + 1, self.cfg.hidden_size * 2)
+        nd_one = candidates_news_ID
         # print(nt_one.size(), ne_two.size(), nt_two.size())
         ut_one = self.his_news_att(his_news_info)
         ue_one = target_user
         ue_two = self.neighbor_user_att(neighbor_user)
+        ud_one = self.news_ID_att(his_news_ID)
         # print(ut_one.size(), ue_one.size(), ue_two.size())
 
-        news_represent = torch.cat([nt_two, ne_two, nt_one], dim=-1)
-        user_represent = torch.cat([ue_one, ue_two, ut_one], dim=-1)
+        news_represent = torch.cat([nt_two, ne_two, nt_one, nd_one], dim=-1)
+        user_represent = torch.cat([ue_one, ue_two, ut_one, ud_one], dim=-1)
+        # user_represent = torch.cat([ut_one, ue_one, ue_two], dim=-1)
 
         user_represent = user_represent.repeat(1, neg_num + 1).view(-1, neg_num + 1, news_represent.size(-1))
         similarity = torch.sum(user_represent * news_represent, dim=-1)
 
         return similarity
-
-        
