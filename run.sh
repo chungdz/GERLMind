633a73eada1d4af09483bc026b10272ac405f7ba
Yunfan Hu <1033718596@qq.com>
2021-01-20 12:37:46 +0800

    news_true

--- run.sh
+++ run.sh
@@ -16,7 +16,7 @@ python -m data_prepocess.find_neighbors
 CUDA_VISIBLE_DEVICES=1,2,3,7 python training.py --gpus=4 --epoch=4
 
 python -m data_prepocess.build_dict --title_len=30
-python -m data_prepocess.find_neighbors --user=False --news=True
+python -m data_prepocess.find_neighbors --news=True
 python -m data_prepocess.build_train --processes=10 --max_hist_length=50
 python -m data_prepocess.resplit --filenum 10 --processes 4
 python -m data_prepocess.build_valid --processes=10 --fsamples=valid/behaviors.small.tsv --max_hist_length=50
